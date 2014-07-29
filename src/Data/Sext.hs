c3017220cd7a90175e954054f9fc127086971b97
Dmitry Dzhus <dima@dzhus.org>
2014-07-29 21:35:36 +0400

    Sextable class

--- src/Data/Sext.hs
+++ src/Data/Sext.hs
@@ -1,41 +1,80 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Data.Sext
+       ( Sextable(..)
+       , padLeft
+       , padRight
+       )
 
 where
 
-import Data.Proxy
-import Data.String
-import Data.Text (Text)
+import           Prelude (($), (-))
+import qualified Prelude as P
+
+import           Data.Proxy
 import qualified Data.Text as T
-import GHC.TypeLits
+
+import           GHC.TypeLits
 
 
-data Sext (a :: Nat) = Sext Text deriving Show
+class Sextable a where
+  data SC a (i :: Nat)
+  type Elem a
+
+  unsafeCreate :: a -> SC a i
+  unwrap :: SC a i -> a
+
+  length :: KnownNat m => SC a m -> P.Integer
+
+  append :: SC a m -> SC a n -> SC a (m + n)
+  replicate :: KnownNat m => Elem a -> SC a m
+  map :: (Elem a -> Elem a) -> SC a m -> SC a m
+
+  take :: (KnownNat m, n <= m) => SC a m -> SC a n
+  drop :: (KnownNat m, n <= m) => SC a m -> SC a n
 
 
--- | Unsafely construct a Sext.
-unsafeSext :: Text -> Sext a
-unsafeSext = Sext
+padLeft :: forall a m n.
+           (Sextable a, KnownNat m, KnownNat (n - m),
+            n ~ (n - m + m), m <= n) =>
+           Elem a -> SC a m -> SC a n
+padLeft pad = append (replicate pad)
 
 
-length :: forall n. KnownNat n => Sext n -> Integer
-length _ = natVal (undefined :: Proxy n)
+padRight :: forall a m n.
+           (Sextable a, KnownNat m, KnownNat (n - m),
+            n ~ (m + (n - m)), m <= n) =>
+           Elem a -> SC a m -> SC a n
+padRight pad = P.flip append (replicate pad)
 
 
-append :: Sext m -> Sext n -> Sext (m + n)
-append (Sext a) (Sext b) = Sext $ T.append a b
+instance Sextable T.Text where
+  data SC T.Text i = Text T.Text
+  type Elem T.Text = P.Char
 
+  unsafeCreate = Text
 
-map :: (Char -> Char) -> Sext m -> Sext m
-map f (Sext s) = Sext $ T.map f s
+  unwrap (Text s) = s
 
+  length (_ :: SC T.Text n) = natVal (Proxy :: Proxy n)
 
-take :: forall n m. (KnownNat n, n <= m) =>
-        Sext m -> Sext n
-take (Sext s) = Sext $ T.take (fromIntegral $ natVal (undefined :: Proxy n)) s
+  append (Text a) (Text b) = Text (T.append a b)
+
+  replicate :: forall m. KnownNat m => Elem T.Text -> SC T.Text m
+  replicate c =
+    Text $ T.replicate (P.fromIntegral $ natVal (Proxy :: Proxy m)) $ T.singleton c
+
+  map f (Text s) = Text (T.map f s)
+
+  take (Text s :: SC T.Text m) =
+    Text $ T.take (P.fromIntegral $ natVal (Proxy :: Proxy m)) s
+
+  drop (Text s :: SC T.Text m) =
+    Text $ T.drop ((T.length s) - (P.fromIntegral $ natVal (Proxy :: Proxy m))) s
