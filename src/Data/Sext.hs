586b200a5b1b57d39d5a43b25f5678448dd20de2
Dmitry Dzhus <dima@dzhus.org>
2014-08-07 04:13:44 +0400

    Bind underlying type operations using Sextable class, drop all TH macros
    More docs

--- src/Data/Sext.hs
+++ src/Data/Sext.hs
@@ -6,41 +6,162 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Data.Sext
-       ( Sextable(..)
-       , length
+       (
+         -- * Constructing Sexts
+         --
+         -- | See also 'C.unsafeCreate'
+         createLeft
+       , createRight
+       , sext
+       , create
+       , replicate
+
+         -- * Working with Sexts
+       , append
+       , take
+       , drop
+       , map
        , padLeft
        , padRight
-       , sext
+
+       , length
+
+         -- * Sextable class
+       , Sextable(C.unsafeCreate, C.unwrap)
        )
 
 where
 
-import qualified Prelude as P
-
-#ifdef WITH_BS
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as LB
-import           GHC.Word
-#endif
-
-#ifdef WITH_TEXT
-import qualified Data.Text as T
-#endif
+import           Prelude as P hiding (drop, length, map, replicate, take)
+import qualified Prelude as P hiding (length)
 
 import           GHC.TypeLits
 
-import           Language.Haskell.TH
-
 import           Data.Proxy
-import           Data.Sext.Class
+import           Data.Sext.Class (Elem, Sext, Sextable)
+import qualified Data.Sext.Class as C
 import           Data.Sext.TH
 
 
+-- | Safely create a Sext, possibly altering the source to match
+-- target length. If target length is less than that of the source,
+-- it gets truncated. If target length is greater, the source is
+-- padded using the provided basic element. Elements on the left are
+-- preferred.
+--
+-- >>> createLeft ' ' "foobarbaz" :: Sext 6 String
+-- "foobar"
+-- >>> createLeft '#' "foobarbaz" :: Sext 12 String
+-- "foobarbaz###"
+createLeft :: forall a i.
+              (Sextable a, KnownNat i) =>
+              Elem a -> a -> Sext i a
+createLeft e s =
+  C.unsafeCreate $
+  C.take (C.length s) $
+  C.append s $
+  C.replicate (t - C.length s) e
+  where
+    t = fromIntegral $ natVal (Proxy :: Proxy i)
+
+
+-- | Just like 'createLeft', except that elements on the right are preferred.
+--
+-- >>> createRight '@' "foobarbaz" :: Sext 6 String
+-- "barbaz"
+-- >>> createRight '!' "foobarbaz" :: Sext 12 String
+-- "!!!foobarbaz"
+createRight :: forall a i.
+               (Sextable a, KnownNat i) =>
+               Elem a -> a -> Sext i a
+createRight e s =
+  C.unsafeCreate $
+  C.drop (C.length s - t) $
+  C.append (C.replicate (t - C.length s) e) s
+  where
+    t = fromIntegral $ natVal (Proxy :: Proxy i)
+
+
+-- | Attempt to safely create a Sext if it matches target length.
+--
+-- >>> create "foobar" :: Maybe (Sext 6 String)
+-- Just "foobar"
+-- >>> create "barbaz" :: Maybe (Sext 8 String)
+-- Nothing
+--
+-- This is safer than 'unsafeCreate' and unlike with 'createLeft' /
+-- 'createRight' the source value is left unchanged. However, this
+-- implies a further run-time check for Nothing values.
+create :: forall a i.
+          (Sextable a, KnownNat i) =>
+          a -> P.Maybe (Sext i a)
+create s =
+  if C.length s == t
+  then Just $ C.unsafeCreate s
+  else Nothing
+  where
+    t = fromIntegral $ natVal (Proxy :: Proxy i)
+
+
+-- | Append two Sexts together.
+--
+-- >>> append "foo" "bar" :: Sext 6 String
+-- "foobar"
+append :: forall a m n.
+          (Sextable a) => Sext m a -> Sext n a -> Sext (m + n) a
+append a b = C.unsafeCreate $ C.append (C.unwrap a) (C.unwrap b)
+
+
+-- | Construct a new Sext from a basic element.
+--
+-- >>> replicate '=' :: Sext 10 String
+-- "=========="
+replicate :: forall a i.
+             (Sextable a, KnownNat i) => Elem a -> Sext i a
+replicate e =
+  C.unsafeCreate $ C.replicate t e
+  where
+    t = fromIntegral $ natVal (Proxy :: Proxy i)
+
+
+map :: Sextable a =>
+       (Elem a -> Elem a) -> Sext m a -> Sext m a
+map f s =
+  C.unsafeCreate $ C.map f $ C.unwrap s
+
+
+-- | Reduce Sext length, preferring elements on the left.
+--
+-- >>> take "Foobar" :: Sext 3 String
+-- "Foo"
+take :: forall a m n.
+        (Sextable a, KnownNat m, KnownNat n, n <= m) =>
+        Sext m a -> Sext n a
+take s =
+  C.unsafeCreate $ C.take t $ C.unwrap s
+  where
+    t = fromIntegral $ natVal (Proxy :: Proxy n)
+
+
+-- | Reduce Sext length, preferring elements on the right.
+--
+-- >>> drop "Foobar" :: Sext 2 String
+-- "ar"
+drop :: forall a m n.
+        (Sextable a, KnownNat m, KnownNat n, n <= m) =>
+        Sext m a -> Sext n a
+drop s =
+  C.unsafeCreate $ C.drop (C.length s' - t) s'
+  where
+    s' = C.unwrap s
+    t = fromIntegral $ natVal (Proxy :: Proxy n)
+
+
+-- | Obtain value-level length.
 length :: forall a m.
           KnownNat m => Sext m a -> P.Int
 length _ = P.fromIntegral P.$ natVal (Proxy :: Proxy m)
@@ -58,59 +179,3 @@ padRight :: forall a m n.
             n ~ (m + (n - m)), m <= n) =>
            Elem a -> Sext m a -> Sext n a
 padRight pad = P.flip append (replicate pad)
-
-
-mkSextable
-  (AppT ListT P.. VarT)
-  (VarT)
-  (mkName "List")
-  'P.length
-  '(P.++)
-  'P.replicate
-  'P.map
-  'P.take
-  'P.drop
-
-
-#ifdef WITH_TEXT
-textReplicate :: P.Int -> P.Char -> T.Text
-textReplicate n c = T.replicate n P.$ T.singleton c
-
-
-mkSextable
-  (\_ -> ConT ''T.Text)
-  (\_ -> ConT ''P.Char)
-  (mkName "Text")
-  'T.length
-  'T.append
-  'textReplicate
-  'T.map
-  'T.take
-  'T.drop
-#endif
-
-
-#ifdef WITH_BS
-mkSextable
-  (\_ -> ConT ''B.ByteString)
-  (\_ -> ConT ''Word8)
-  (mkName "ByteString")
-  'B.length
-  'B.append
-  'B.replicate
-  'B.map
-  'B.take
-  'B.drop
-
-
-mkSextable
-  (\_ -> ConT ''LB.ByteString)
-  (\_ -> ConT ''Word8)
-  (mkName "LazyByteString")
-  'LB.length
-  'LB.append
-  'LB.replicate
-  'LB.map
-  'LB.take
-  'LB.drop
-#endif
--- src/Data/Sext/Class.hs
+++ src/Data/Sext/Class.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
+
+{-|
+
+Use this only if you need to make some type Sextable.
+
+-}
 
 module Data.Sext.Class
        ( Sextable(..)
@@ -8,7 +14,17 @@ module Data.Sext.Class
 
 where
 
+import           Prelude
 import qualified Prelude as P
+
+#ifdef WITH_BS
+import qualified Data.ByteString as B
+import           GHC.Word
+#endif
+
+#ifdef WITH_TEXT
+import qualified Data.Text as T
+#endif
 
 import           GHC.TypeLits
 
@@ -18,43 +34,10 @@ class Sextable a where
   -- | Data family which wraps values of the underlying type giving
   -- them a type-level length. @Sext 6 t@ means a value of type @t@ of
   -- length 6.
-  --
-  -- In examples in this class, we omit constructor names of this type.
   data Sext (i :: Nat) a
+
   -- | Basic element type. For @Sextable [a]@, this is @a@.
   type Elem a
-
-  -- | Safely create a Sext, possibly altering the source to match
-  -- target length. If target length is less than that of the source,
-  -- it gets truncated. If target length is greater, the source is
-  -- padded using the provided basic element. Elements on the left are
-  -- preferred.
-  --
-  -- >>> createLeft ' ' "foobarbaz" :: Sext 6 String
-  -- "foobar"
-  -- >>> createLeft '#' "foobarbaz" :: Sext 12 String
-  -- "foobarbaz###"
-  createLeft :: (KnownNat i) => Elem a -> a -> Sext i a
-
-  -- | Just like 'createLeft', but elements on the right are preferred.
-  --
-  -- >>> createRight '@' "foobarbaz" :: Sext 6 String
-  -- "barbaz"
-  -- >>> createRight '!' "foobarbaz" :: Sext 12 String
-  -- "!!!foobarbaz"
-  createRight :: (KnownNat i) => Elem a -> a -> Sext i a
-
-  -- | Attempt to safely create a Sext if it matches target length.
-  --
-  -- >>> create "foobar" :: Maybe (Sext 6 String)
-  -- Just "foobar"
-  -- >>> create "barbaz" :: Maybe (Sext 8 String)
-  -- Nothing
-  --
-  -- This is safer than 'unsafeCreate' and unlike with 'createLeft' /
-  -- 'createRight' the source value is left unchanged. However, this
-  -- implies a further run-time check for Nothing values.
-  create :: (KnownNat i) => a -> P.Maybe (Sext i a)
 
   -- | Simply wrap a value in a Sext as is, assuming any length.
   --
@@ -67,31 +50,73 @@ class Sextable a where
   -- all functions defined for Sext.
   --
   -- Use it only when you know what you're doing.
+  --
+  -- When implementing new Sextable instances, code this to simply
+  -- apply the constructor of 'Sext'.
   unsafeCreate :: a -> Sext i a
 
-  -- | Forget type-level length.
+  -- | Forget type-level length, obtaining the underlying value.
   unwrap :: Sext i a -> a
 
-  append :: Sext m a -> Sext n a -> Sext (m + n) a
+  length :: a -> Int
+  append :: a -> a -> a
+  replicate :: Int -> Elem a -> a
+  map :: (Elem a -> Elem a) -> a -> a
+  take :: Int -> a -> a
+  drop :: Int -> a -> a
 
-  -- | Construct a new Sext from a basic element.
-  --
-  -- >>> replicate '=' :: Sext 10 String
-  -- "=========="
-  replicate :: KnownNat m => Elem a -> Sext m a
 
-  map :: (Elem a -> Elem a) -> Sext m a -> Sext m a
+instance (Show a, Sextable a) => Show (Sext i a) where
+  show s = show $ unwrap s
 
-  -- | Reduce Sext length, preferring elements on the left.
-  --
-  -- >>> take "Foobar" :: Sext 3 String
-  -- "Foo"
-  take :: (KnownNat m, KnownNat n, P.True ~ (<=?) n m) => Sext m a -> Sext n a
-  -- | Reduce Sext length, preferring elements on the right.
-  --
-  -- >>> drop "Foobar" :: Sext 2 String
-  -- "ar"
-  drop :: (KnownNat m, KnownNat n, P.True ~ (<=?) n m) => Sext m a -> Sext n a
 
-instance (P.Show a, Sextable a) => P.Show (Sext i a) where
-  show s = P.show P.$ unwrap s
+instance Sextable [a] where
+  type Elem [a] = a
+
+  data Sext i [a] = List [a]
+
+  unsafeCreate = List
+  unwrap (List l) = l
+
+  length = P.length
+  append = (P.++)
+  replicate = P.replicate
+  map = P.map
+  take = P.take
+  drop = P.drop
+
+
+#ifdef WITH_TEXT
+instance Sextable T.Text where
+  type Elem T.Text = Char
+
+  data Sext i T.Text = Text T.Text
+
+  unsafeCreate = Text
+  unwrap (Text t) = t
+
+  length = T.length
+  append = T.append
+  replicate = \n c -> T.replicate n (T.singleton c)
+  map = T.map
+  take = T.take
+  drop = T.drop
+#endif
+
+
+#ifdef WITH_BS
+instance Sextable B.ByteString where
+  type Elem B.ByteString = Word8
+
+  data Sext i B.ByteString = ByteString B.ByteString
+
+  unsafeCreate = ByteString
+  unwrap (ByteString t) = t
+
+  length = B.length
+  append = B.append
+  replicate = B.replicate
+  map = B.map
+  take = B.take
+  drop = B.drop
+#endif
